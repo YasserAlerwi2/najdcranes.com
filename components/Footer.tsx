33d8985f69cfd8bb27b4b17cebbb041bbafa313f
YasserAlerwi2 <yasseralerwi458@gmail.com>
2025-07-19 17:00:59 +0300

    Add second phone number to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -123,6 +123,16 @@ export default function Footer() {
               <div className="flex items-center space-x-3">
                 <Phone className="h-5 w-5 text-orange-400 flex-shrink-0" />
                 <a
+                  href="tel:+966568810306"
+                  className="text-gray-300 hover:text-orange-400 transition-colors"
+                >
+                  +966 56 881 0306
+                </a>
+              </div>
+
+              <div className="flex items-center space-x-3">
+                <Phone className="h-5 w-5 text-orange-400 flex-shrink-0" />
+                <a
                   href="tel:+966555403290"
                   className="text-gray-300 hover:text-orange-400 transition-colors"
                 >
@@ -136,7 +146,7 @@ export default function Footer() {
                   href="mailto:info@najdcranes.com"
                   className="text-gray-300 hover:text-orange-400 transition-colors"
                 >
-                  info@najdcranes.com
+                  m.c.najd.est@gmail.com
                 </a>
               </div>
             </div>
